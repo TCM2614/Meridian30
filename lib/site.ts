e2128f327d672b5d678770d7abe80e1aee2dea39
chant3azy <chantie67@gmail.com>
2026-06-13 19:11:09 +0100

    Change team member name to Daniella Okocha
    Updated the name from 'Daniel Okocha' to 'Daniella Okocha' in the team member details.

--- lib/site.ts
+++ lib/site.ts
@@ -256,7 +256,7 @@ export const leadership = [
     bio: "",
   },
   {
-    name: "Daniel Okocha",
+    name: "Daniella Okocha",
     role: "Director - Consultancy, Tender and Bid Writing",
     bio: "Daniella Okocha is a bid and business strategy professional with expertise spanning commercial operations, legal advisory, governance, and business transformation. Combining academic excellence in law, business, and management with hands-on experience across the UK and Africa, she brings a unique ability to translate complex challenges into practical, results-driven solutions. Her analytical thinking, stakeholder engagement, and strategic execution has successfully improved bid performance, enhanced organisational productivity, strengthened governance frameworks, and delivered process optimisation initiatives that create measurable business value. She offers organisations a rare blend of legal insight, commercial awareness, and operational expertise, enabling teams to improve efficiency, mitigate risk and drive growth.
 ",
